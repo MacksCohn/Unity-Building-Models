6d2ad85fabb9fcaaa06998663e12af6edff471d2
MacksCohn <maxcohn@live.com>
2024-08-15 11:26:24 -0400

    Added zoom with scroll wheel

--- orbit_component.cs
+++ orbit_component.cs
@@ -7,15 +7,17 @@ using UnityEngine;
 public class orbit_component : MonoBehaviour
 {
     public new Camera camera;
-    public float sensitivity = 1200;
+    public float mouse_sensitivity = 1200;
     public float max_angle = 60f;
     public float min_angle = 0f;
+    public float scroll_sensitivity = 2000;
 
     void Start()
     {
-        sensitivity = 1200;
+        mouse_sensitivity = 1200;
         max_angle = 60f;
         min_angle = 0f;
+        scroll_sensitivity = 2000;
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -26,19 +28,14 @@ public class orbit_component : MonoBehaviour
     {
         float orbit_spin_amount = Input.GetAxis("Mouse X");
         float orbit_height_amount = Input.GetAxis("Mouse Y");
-        camera.transform.RotateAround(Vector3.zero, Vector3.up, orbit_spin_amount * sensitivity * Time.deltaTime);
-        // NEED TO rotate up and down axis independent somehow locally
+        camera.transform.RotateAround(Vector3.zero, Vector3.up, orbit_spin_amount * mouse_sensitivity * Time.deltaTime);
         Vector3 away = -camera.transform.position;
         Vector3 axis = Vector3.Cross(away, Vector3.up);
-        // print(camera.transform.localEulerAngles.x);
-        if (orbit_height_amount > 0)
-            print("up " + (camera.transform.localEulerAngles.x).ToString());
-        else if (orbit_height_amount < 0)
-            print ("down " + (camera.transform.localEulerAngles.x).ToString());
-        // Don't have easy way to lookahead for rotate around
-        if ((orbit_height_amount > 0 && camera.transform.localEulerAngles.x  - orbit_height_amount * sensitivity * Time.deltaTime > min_angle) && // up moves cam up and view down
-            !(orbit_height_amount > 0 && camera.transform.localEulerAngles.x - orbit_height_amount * sensitivity * Time.deltaTime > max_angle) ||
-            (orbit_height_amount < 0 && camera.transform.localEulerAngles.x -  orbit_height_amount * sensitivity * Time.deltaTime < max_angle)) 
-            camera.transform.RotateAround(Vector3.zero, axis, orbit_height_amount * sensitivity * Time.deltaTime);
+        if ((orbit_height_amount > 0 && camera.transform.localEulerAngles.x  - orbit_height_amount * mouse_sensitivity * Time.deltaTime > min_angle) && // up moves cam up and view down
+            !(orbit_height_amount > 0 && camera.transform.localEulerAngles.x - orbit_height_amount * mouse_sensitivity * Time.deltaTime > max_angle) ||
+            (orbit_height_amount < 0 && camera.transform.localEulerAngles.x -  orbit_height_amount * mouse_sensitivity * Time.deltaTime < max_angle)) 
+            camera.transform.RotateAround(Vector3.zero, axis, orbit_height_amount * mouse_sensitivity * Time.deltaTime);
+    
+        camera.fieldOfView -= Input.mouseScrollDelta.y * scroll_sensitivity * Time.deltaTime;
     }
 }
